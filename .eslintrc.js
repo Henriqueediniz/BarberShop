1056764d781249a7a94c5fd090a6b14ea3015914
Henrique Diniz <henriquee.diniz@hotmail.com>
2019-12-14 00:57:00 +1100

    adding eslint and standardizing configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,21 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: ["airbnb-base"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "airbnb-base"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
 };
