312595848350e9017bfcc3fa797bac0c4d28fe44
Henrique Diniz <henriquee.diniz@hotmail.com>
2019-12-15 16:39:06 +1100

    sequelize config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,23 @@
 module.exports = {
   env: {
     es6: true,
-    node: true,
+    node: true
   },
-  extends: ["airbnb-base"],
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
-  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
+    sourceType: 'module'
   },
-  plugins: ["@typescript-eslint"],
-  rules: {},
+  rules: {
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }]
+  }
 };
