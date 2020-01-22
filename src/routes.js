3b8576ea40c5a33c18e2e819246262bcc86f0b7f
Henrique Diniz <henriquee.diniz@hotmail.com>
2020-01-22 23:50:24 +1100

    add new routes and validate session

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,17 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 
 import SessionController from './app/controllers/SessionController';
+import authMiddlewares from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+// using the auth to validate session
+
+routes.use(authMiddlewares);
 routes.put('/users', UserController.update);
 
+// --
 export default routes;
