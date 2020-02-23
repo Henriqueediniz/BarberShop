a6f3bec842cb0689b7099d0f3209692c768592ed
Henrique Diniz <henriquee.diniz@hotmail.com>
2020-02-23 23:04:23 +1100

    change user fields

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+
 import authMiddlewares from './app/middlewares/auth';
 
 const routes = new Router();
@@ -19,7 +21,6 @@ routes.put('/users', UserController.update);
 
 // --
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
+
 export default routes;
